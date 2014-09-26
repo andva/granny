c9b93c514d2aa03d246c1ecebc930a29a5a4a103
Valross <hamax109@student.liu.se>
2014-09-26 22:07:07 +0200

    Changed ball position on images

--- drawer.py
+++ drawer.py
@@ -3,4 +3,4 @@ from pygame.locals import *
 
 def drawImage(image, screen, screenPosition):
 	img = pygame.image.load(image)
-	screen.blit(img,(screenPosition[0],screenPosition[1]))
+	screen.blit(img,(screenPosition[0] - img.get_rect().size[0] / 2,screenPosition[1] - img.get_rect().size[1] + img.get_rect().size[1] / 4))
